700c8a8074499f77f7b83b523bf58289ad4840b1
Kalyan Singh <kalyansingh6030@gmail.com>
2020-10-19 00:34:12 +0530

    make changes in Chatter

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_04_21_072317) do
 
-  create_table "room_messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "room_messages", force: :cascade do |t|
     t.bigint "room_id"
     t.bigint "user_id"
     t.text "message"
@@ -22,13 +25,13 @@ ActiveRecord::Schema.define(version: 2020_04_21_072317) do
     t.index ["user_id"], name: "index_room_messages_on_user_id"
   end
 
-  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
